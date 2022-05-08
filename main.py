7eb7b6ab91b0bdce5d56a2ae6005f640437b31ef
inAsees <gurasees.singh121@gmail.com>
2022-05-08 15:01:22 +0530

    Some imports like json, beautifulsoup. 1. New methods __init__ and  get_beverage_url created

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+import requests as req
+import json
+from bs4 import BeautifulSoup as bs
 
 
 @dataclass
@@ -15,4 +18,18 @@ class BeverageInfo:
 
 class Scrapper:
     def __init__(self):
-        pass
+        self._base_url = "https://www.carrefour.ke"
+        self._category_beverages_url = self._base_url + "/mafken/en/c/FKEN1500000"
+
+    def get_beverage_url(self, base_url: str):
+        with open("base_url.html", "r", encoding="UTF-8") as f:
+            page_soup = bs(f, "html.parser")
+        page_txt = page_soup.find("script", {"id": "__NEXT_DATA__"}).text
+        page_json = json.loads(page_txt)
+        for url in page_json["props"]["initialState"]["search"]["products"]:
+            beverage_url = base_url + url["url"]
+            pass
+
+
+if __name__ == "__main__":
+    pass
